f8d5b7bd473c78e56030b28439cb264fa4aaee1b
Catherine Salazar <catcreates13@gmail.com>
2018-06-24 21:18:44 +0000

    testing pseudocode

--- lib/game_of_thrones_houses/cli.rb
+++ lib/game_of_thrones_houses/cli.rb
@@ -2,6 +2,27 @@ class GameOfThronesHouses::CLI
   #send and get data to/from user
 
   def call
-    puts "Hello"
+    puts "Greetings!"
+    puts ""
+    menu
   end
+
+  def menu
+    puts "Please choose a number for your house"
+    puts "1. House Targaryen of King's Landing"
+    puts "2. House Tarly of Horn Hill"
+    puts "3. House Tarth of Evenfall Hall"
+
+    input = gets.strip.to_i
+    if input == 1
+      puts "House Targaryen of King's Landing"
+    elsif input == 2
+      puts "House Tarly of Horn Hill"
+    elsif input == 3
+      puts "House Tarth of Evenfall Hall"
+    else
+      puts "I don't know that choice"
+      menu
+  end
+end
 end
