3b5114a6d4bc08c10fa39eb9abfcd07f200539f5
Catherine Salazar <catcreates13@gmail.com>
2018-07-03 02:05:32 +0000

    added more methods to extend functionality

--- lib/game_of_thrones_houses/cli.rb
+++ lib/game_of_thrones_houses/cli.rb
@@ -3,33 +3,54 @@ class GameOfThronesHouses::CLI
 
   def call
     GameOfThronesHouses::API.house_list
-    puts "Greetings!"
     puts ""
+    puts "M’athchomaroon!"
+    puts ""
+    puts "Here are some Game of Thrones houses:"
     house_list
     menu
+    goodbye
   end
 
   def house_list
     GameOfThronesHouses::HOUSE.all.each.with_index(1) do |house, i|
       puts "#{i}. #{house.name}"
     end
-    end
+      puts ""
+      puts "What house do you want to see?"
+  end
 
 #if input in invalid, say something, return to menu
 #if input is correct, show more info
 #exit option
   def menu
-    puts "What house do you want to see?"
+    input = nil
     puts ""
     input = gets.strip.to_i
     house = GameOfThronesHouses::HOUSE.find(input)
     house_details(house)
+    puts ""
+    puts "Would you like to see another house? Enter Y or N"
+    input == gets.strip.downcase
+    if input == "y"
+      menu
+    else
+      goodbye
   end
+end
 
   def house_details(house)
     puts ""
     puts "#{house.name}"
     puts "#{house.region}"
     puts "#{house.coatOfArms}"
+  end
+
+  def error
+    puts "Hodor. Hodor. Hodor."
+  end
+
+  def goodbye
+    "Goodbye. And remember.... winter is coming."
   end
 end
